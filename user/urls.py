b6aafb0237385e9454b353bd4f7e615ab19e42e7
nhan-skytech <Nhan.TCH@skytechkey.com>
2021-04-15 16:00:02 +0700

    custom password reset page

--- user/urls.py
+++ user/urls.py
@@ -22,12 +22,11 @@ urlpatterns = [
 		template_name='registration/password_change_done.html'), name='password_change_done'),
 	path('password_reset/', auth_views.PasswordResetView.as_view(
 		template_name='registration/password_reset.html',
-		html_email_template_name="registration/password_reset_email.html",
-		email_template_name="registration/password_reset_email.html",), name='password_reset'),
+		html_email_template_name="registration/html_password_reset_email.html"), name='password_reset'),
 	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
 		template_name='registration/password_reset_sent.html'), name='password_reset_done'),
 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-		template_name='registration/password_reset_form.html'), name='password_reset_confirm'),
+		template_name='registration/html_password_reset_confirm.html'), name='password_reset_confirm'),
 	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
 		template_name='registration/password_reset_sent.html'), name='password_reset_complete'),
 
