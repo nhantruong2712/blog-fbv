50a90b96c00ba9a77e0faec1384c8833a35f6c8e
nhan-skytech <Nhan.TCH@skytechkey.com>
2021-04-16 09:38:53 +0700

    custom change password and edit password reset

--- user/urls.py
+++ user/urls.py
@@ -17,9 +17,9 @@ urlpatterns = [
 
 	# custom interface auth_views
 	path('password_change/', auth_views.PasswordChangeView.as_view(
-		template_name='registration/password_change.html'), name='password_change'),
+		template_name='registration/html_password_change_form.html'), name='password_change'),
 	path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-		template_name='registration/password_change_done.html'), name='password_change_done'),
+		template_name='registration/html_password_change_done.html'), name='password_change_done'),
 	path('password_reset/', auth_views.PasswordResetView.as_view(
 		template_name='registration/password_reset.html',
 		html_email_template_name="registration/html_password_reset_email.html"), name='password_reset'),
@@ -28,7 +28,7 @@ urlpatterns = [
 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
 		template_name='registration/html_password_reset_confirm.html'), name='password_reset_confirm'),
 	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-		template_name='registration/password_reset_sent.html'), name='password_reset_complete'),
+		template_name='registration/html_password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 
