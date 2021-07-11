a18eb239f17ed8133b6fc1e85888eb52693d166d
nhantruong2712 <trconghiennhan2712@gmail.com>
2021-07-11 16:35:34 +0700

    update

--- blogfbv/settings.py
+++ blogfbv/settings.py
@@ -6,7 +6,6 @@ except ImportError:
 import os
 import django_heroku
 import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
